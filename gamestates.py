14c64b915cbb066e80ad1f4a7c7cd80d18930f53
Satwik Behera <satwikbehera12@gmail.com>
2024-06-23 19:09:35 -0700

    added smooth life
    I added smooth life, but I'm figuring out how the transformation function works, right now, it is very chaotic given the current parameters

---
+++ gamestates.py
@@ -0,0 +1,71 @@
+import pygame
+import sys
+
+SCREENWIDTH, SCREENHEIGHT = 1280, 720
+FPS = 60
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+        self.clock = pygame.time.Clock()
+
+        self.gameStateManager = GameStateManager('start')
+        self.start = Start(self.screen, self.gameStateManager)
+        self.level = Level(self.screen, self.gameStateManager)
+
+        self.states = {'start': self.start, 'level': self.level}
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    self.gameStateManager.set_state('level')
+
+            self.states[self.gameStateManager.get_state()].run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+class Level:
+    def __init__(self, display, gameStateManager):
+        self.display = display
+        self.gameStateManager = gameStateManager
+
+    def run(self):
+        self.display.fill('blue')
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_e]:
+            self.gameStateManager.set_state('start')
+
+
+class Start:
+    def __init__(self, display, gameStateManager):
+        self.display = display
+        self.gameStateManager = gameStateManager
+
+    def run(self):
+        self.display.fill('red')
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_e]:
+            self.gameStateManager.set_state('level')
+
+
+class GameStateManager:
+    def __init__(self, currentState):
+        self.currentState = currentState
+
+    def get_state(self):
+        return self.currentState
+
+    def set_state(self, state):
+        self.currentState = state
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
--- smoothlife.py
+++ smoothlife.py
@@ -8,10 +8,10 @@ Game of life has 4 rules :
 
 # importing libraries
 import pygame
+import pygame_widgets
+from pygame_widgets.slider import Slider
 from pygame_widgets.textbox import TextBox
 import random
-from pygame.gfxdraw import filled_circle
-import raster_geometry
 import numpy as np
 
 # initialize the pygame module so we can utilize it
@@ -27,26 +27,27 @@ WIDTH, HEIGHT = 1280, 720  # height and width of the screen where we'll play the
 TILE_SIZE = 10  # width and height of each individual grid box
 GRID_WIDTH = WIDTH // TILE_SIZE  # number of tiles we'll have across the width
 GRID_HEIGHT = HEIGHT // TILE_SIZE  # number of tiles we'll have across the height of the screen
-FPS = 60
+FPS = 60  # the while loop runs this many times per second
 
 # now the screen is where we'll be doing all our pygame drawing. This is how we initialize a new pygame window
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+slider = Slider(screen, 400, 20, 200, 15, min=4, max=20, step=1)
 output = TextBox(screen, 1025, 5, 50, 50, fontSize=30)
 
 clock = pygame.time.Clock()
 
 
 def gen(num):
-    return set([(random.randrange(0, GRID_WIDTH), random.randrange(0, GRID_HEIGHT)) for _ in range(num)])
+    return set([(random.randrange(0, GRID_WIDTH), random.randrange(0, GRID_HEIGHT), np.round(random.random(), 1)) for _ in range(num)])
 
 
 def draw_grid(positions):  # it takes in a set of positions where there are live cells, so that for checking the rules,
     # we won't have to loop over every cell
     for position in positions:
-        col, row = position  # our position is indicated by column number and a row number
+        col, row, in10ct = position  # our position is indicated by column number and a row number
         top_left = (col * TILE_SIZE, row * TILE_SIZE)  # translating the indexes into pixel positions
-        pygame.draw.rect(screen, YELLOW, (*top_left, TILE_SIZE, TILE_SIZE))
+        pygame.draw.rect(screen, tuple([in10ct*x for x in YELLOW]), (*top_left, TILE_SIZE, TILE_SIZE))
         # the *top_left indicates that we're unpacking the arguments of the tuple, so instead of taking it as a
         # couple, we're considering writing them as 2 separate arguments
 
@@ -63,37 +64,42 @@ def adjust_grid(positions):
     new_positions = set()
 
     for position in positions:
-        neighbors = get_neighbors(position)
+        neighbors = get_inner_neighbors(position)
         all_neighbors.update(neighbors)
 
         neighbors = list(filter(lambda x: x in positions, neighbors))
 
-        if len(neighbors) in [2, 3]:
+        intensities = [i[2] for i in neighbors]
+
+        # A live cell with 2 or 3 live neighbour lives[survival]
+        if 0.2 <= sum(intensities) <= 0.3:
             new_positions.add(position)
 
     for position in all_neighbors:
-        neighbors = get_neighbors(position)
+        neighbors = get_inner_neighbors(position)
         neighbors = list(filter(lambda x: x in positions, neighbors))
 
-        if len(neighbors) == 3:
+        intensities = [i[2] for i in neighbors]
+
+        if 0.3 <= sum(intensities) <= 0.35:
             new_positions.add(position)
 
     return new_positions
 
 
-def get_neighbors(pos):
-    x, y = pos
+def get_inner_neighbors(pos):
+    x, y, i = pos
     neighbors = []
-    for dx in [-1, 0, 1]:
+    for dx in [_ for _ in [-1, 0, 1]]:
         if x + dx < 0 or x + dx > GRID_WIDTH:
             continue
-        for dy in [-1, 0, 1]:
+        for dy in [_ for _ in [-1, 0, 1]]:
             if y + dy < 0 or y + dy > GRID_HEIGHT:
                 continue
             if dx == 0 and dy == 0:
                 continue
 
-            neighbors.append((x + dx, y + dy))
+            neighbors.append((x + dx, y + dy, i))
 
     return neighbors
 
@@ -104,11 +110,12 @@ def main():
     supposed to run constantly in the background checking the logic, and the input, and button presses etc.
     This is where most of the core functions will be called from.
     """
-    running = True
-    playing = False
-    count = 0
-    update_freq = 10
-    iterations = 0
+    running = True  # running represents whether our game is running or not
+    playing = False  # Just determines if YOU'VE paused the game or if it's playing (only to be showed in the title bar)
+    count = 0  # for each loop, counts increases
+    update_freq = 10  # once the count reaches the update frequency, the game is updated
+    # Since our FPS is 60, and our update_frequency is 2, which means game will run on 30 FPS
+    iterations = 0  # To count the number of iterations
 
     positions = set()  # just mentioning that positions is a set of locations where there'll be live cells
     while running:  # this is our main loop
@@ -136,7 +143,8 @@ def main():
                 x, y = pygame.mouse.get_pos()  # get the positions for the moues click
                 col = x // TILE_SIZE  # figuring out the tile position from the mouse coordinates
                 row = y // TILE_SIZE
-                pos = (col, row)
+                intensity = 1
+                pos = (col, row, intensity)
 
                 # If there's already this position in the set of positions, then it'll remove that positions,
                 # else add it
@@ -162,7 +170,8 @@ def main():
         draw_grid(positions)  # and then we're going to draw the grid.
         score_text = font.render(f'Iterations: {iterations}', True, (255, 255, 255))
         screen.blit(score_text, (10, 10))
-        filled_circle(screen, 400, 400, 50, (0, 0, 255))
+        output.setText(slider.getValue())
+        pygame_widgets.update(event)
         pygame.display.update()  # and then we'll update the display
         # The above three commands needs to be in order. If we wrote draw_grid, and then screen.fill, then you'll
         # only see a gray screen because pygame would draw the grid, and then fill the screen with GREY
@@ -171,5 +180,4 @@ def main():
 
 
 if __name__ == "__main__":
-    #main()
-    print(np.round(raster_geometry.circle(20, 9, smoothing=1.0), 2) - np.round(raster_geometry.circle(20, 3, smoothing=0.5), 2))
+    main()
